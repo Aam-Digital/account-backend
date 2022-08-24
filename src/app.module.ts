57cbdd1aae72688883b9ed8c85edb918d8086111
Simon <simon@aam-digital.com>
2022-08-24 09:25:30 +0200

    added account module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { HttpModule } from '@nestjs/axios';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [AuthModule, AuthModule, HttpModule],
+  imports: [AuthModule, AuthModule, HttpModule, AccountModule],
   controllers: [AppController],
 })
 export class AppModule {}
