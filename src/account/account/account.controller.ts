a78fdd94f7cdb503ccb7b9bb8e8c447d4cf6e3d9
Simon <simon@aam-digital.com>
2022-08-27 10:03:20 +0200

    implemented endpoint for adding a email to a account

--- src/account/account/account.controller.ts
+++ src/account/account/account.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Put, Req, UseGuards } from '@nestjs/common';
+import { BearerGuard } from '../../auth/bearer/bearer.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { firstValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
+import { User } from './user.dto';
 
+@UseGuards(BearerGuard)
+@ApiBearerAuth()
 @Controller('account')
-export class AccountController {}
+export class AccountController {
+  constructor(private http: HttpService) {}
+  @Put('set-email')
+  async setEmail(@Req() req, @Body() { email }: User) {
+    const user = req.user;
+    const url = `https://keycloak.aam-digital.com/admin/realms/${user.realm}/users/${user.sub}`;
+    await firstValueFrom(
+      this.http.put(url, { email: email, requiredActions: ['VERIFY_EMAIL'] }),
+    );
+    await firstValueFrom(
+      this.http.put(
+        `${url}/execute-actions-email?client_id=${user.client}&redirect_uri=`,
+        ['VERIFY_EMAIL'],
+      ),
+    );
+    return 'success';
+  }
+}
--- src/auth/bearer/bearer.strategy.ts
+++ src/auth/bearer/bearer.strategy.ts
@@ -12,13 +12,14 @@ export class BearerStrategy extends PassportStrategy(Strategy) {
   }
 
   validate(token): Promise<any> {
-    const { iss } = jwtDecode<{ iss: string; azp: string }>(token);
+    const { iss, azp } = jwtDecode<{ iss: string; azp: string }>(token);
+    const realm = iss.match(/realms\/(.+)$/)[1];
     const url = iss + '/protocol/openid-connect/userinfo';
     return firstValueFrom(
       this.http
         .get(url, { headers: { Authorization: 'Bearer ' + token } })
         .pipe(
-          map((req) => req.data),
+          map((req) => Object.assign(req.data, { realm: realm, client: azp })),
           catchError(() => {
             throw new UnauthorizedException();
           }),
