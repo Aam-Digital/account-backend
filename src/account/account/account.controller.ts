8e5db53b58c093df82f426122e92f87c690ab7c2
Simon <simon@aam-digital.com>
2022-08-27 10:28:22 +0200

    added endpoint to send password forget email

--- src/account/account/account.controller.ts
+++ src/account/account/account.controller.ts
@@ -1,17 +1,26 @@
-import { Body, Controller, Put, Req, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { BearerGuard } from '../../auth/bearer/bearer.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, map } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
-import { User } from './user.dto';
+import { ForgotEmailReq } from './forgot-email-req.dto';
+import { SetEmailReq } from './set-email-req.dto';
 
-@UseGuards(BearerGuard)
-@ApiBearerAuth()
 @Controller('account')
 export class AccountController {
   constructor(private http: HttpService) {}
+  @UseGuards(BearerGuard)
+  @ApiBearerAuth()
   @Put('set-email')
-  async setEmail(@Req() req, @Body() { email }: User) {
+  async setEmail(@Req() req, @Body() { email }: SetEmailReq) {
     const user = req.user;
     const url = `https://keycloak.aam-digital.com/admin/realms/${user.realm}/users/${user.sub}`;
     await firstValueFrom(
@@ -23,6 +32,22 @@ export class AccountController {
         ['VERIFY_EMAIL'],
       ),
     );
+    return 'success';
+  }
+
+  @Post('forgot-email')
+  async forgotEmail(@Body() { email, realm, client }: ForgotEmailReq) {
+    const usersUrl = `https://keycloak.aam-digital.com/admin/realms/${realm}/users`;
+    const getUserUrl = `${usersUrl}?q=email:${email}`;
+    const usersWithEmail = await firstValueFrom(
+      this.http.get<any[]>(getUserUrl).pipe(map((res) => res.data)),
+    );
+    if (usersWithEmail.length !== 1) {
+      throw new BadRequestException(`Could not find user with email: ${email}`);
+    }
+    const user = usersWithEmail[0];
+    const resetPasswordUrl = `${usersUrl}/${user.id}/execute-actions-email?client_id=${client}&redirect_uri=`;
+    await firstValueFrom(this.http.put(resetPasswordUrl, ['UPDATE_PASSWORD']));
     return 'success';
   }
 }
