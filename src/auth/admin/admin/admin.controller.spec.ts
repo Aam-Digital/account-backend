b7bad9b41a396d72605da6d30b9ce3708cd50ecd
Simon <simon@aam-digital.com>
2022-08-29 13:39:25 +0200

    added test for admin controller

--- src/auth/admin/admin/admin.controller.spec.ts
+++ src/auth/admin/admin/admin.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminController } from './admin.controller';
+import { AdminAuthService } from '../admin-auth/admin-auth.service';
 
 describe('AdminController', () => {
   let controller: AdminController;
+  const mockAdminAuth = {
+    login: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AdminController],
+      providers: [{ provide: AdminAuthService, useValue: mockAdminAuth }],
     }).compile();
 
     controller = module.get<AdminController>(AdminController);
@@ -14,5 +19,14 @@ describe('AdminController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call login with provided credentials', () => {
+    controller.authenticate({ username: 'MyUsername', password: 'MyPassword' });
+
+    expect(mockAdminAuth.login).toHaveBeenLastCalledWith(
+      'MyUsername',
+      'MyPassword',
+    );
   });
 });
