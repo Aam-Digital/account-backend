f97af1b60055f98a67c8e620492c25a79e3e76cf
Simon <simon@aam-digital.com>
2022-08-29 16:15:47 +0200

    feat: added error logging (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,51 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { AccountModule } from './account/account.module';
+import { SeverityLevel } from '@sentry/node';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SentryInterceptor, SentryModule } from '@ntegral/nestjs-sentry';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
+const lowSeverityLevels: SeverityLevel[] = ['log', 'info'];
 
 @Module({
-  imports: [AuthModule, AccountModule],
+  providers: [
+    { provide: APP_INTERCEPTOR, useFactory: () => new SentryInterceptor() },
+  ],
+  imports: [
+    AuthModule,
+    AccountModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    SentryModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        if (!configService.get('SENTRY_DSN')) {
+          return;
+        }
+
+        return {
+          dsn: configService.get('SENTRY_DSN'),
+          debug: true,
+          environment: 'prod',
+          release: 'accounts@' + process.env.npm_package_version,
+          whitelistUrls: [/https?:\/\/(.*)\.?aam-digital\.com/],
+          initialScope: {
+            tags: {
+              // ID of the docker container in which this is run
+              hostname: process.env.HOSTNAME || 'unknown',
+            },
+          },
+          beforeSend: (event) => {
+            if (lowSeverityLevels.includes(event.level)) {
+              return null;
+            } else {
+              return event;
+            }
+          },
+        };
+      },
+    }),
+  ],
 })
 export class AppModule {}
