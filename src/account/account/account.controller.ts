377c0c9969072b7e8e7c7a359c8b779771b48789
Simon <simon@aam-digital.com>
2022-09-28 13:54:38 +0200

    added documentation

--- src/account/account/account.controller.ts
+++ src/account/account/account.controller.ts
@@ -17,6 +17,11 @@ import { User } from '../../auth/user.dto';
 import { ConfigService } from '@nestjs/config';
 import { NewAccount } from './new-account.dto';
 
+/**
+ * Endpoints to perform user account related tasks.
+ * These use the Keycloak Admin API {@link https://www.keycloak.org/docs-api/19.0.1/rest-api/index.html}.
+ * The documentation can be found on the swagger UI at `/api`
+ */
 @Controller('account')
 export class AccountController {
   private readonly keycloakUrl: string;
@@ -24,49 +29,59 @@ export class AccountController {
     this.keycloakUrl = configService.get('KEYCLOAK_URL');
   }
 
+  @ApiOperation({
+    summary: 'create a new account',
+    description: `Creates a new account with the provided username and email.
+      The default role 'user_app' is assigned to the user.
+      A email is sent to the provided address to verify the account and set a password.
+    `,
+  })
+  @ApiBearerAuth()
+  @UseGuards(BearerGuard)
   @Put()
-  async createAccount(@Body() { username, email }: NewAccount) {
-    // todo take from user object
-    const realm = 'dev';
-    const client = 'app';
-    const user = {
+  async createAccount(
+    @Req() { user }: { user: User },
+    @Body() { username, email }: NewAccount,
+  ) {
+    // create user
+    const newUser = {
       username,
       attributes: { exact_username: username },
-      email: email,
+      email,
       requiredActions: ['VERIFY_EMAIL'],
       enabled: true,
       credentials: [
         {
           type: 'password',
           value: 'tmpPass',
+          // this triggers the set new password flow
           temporary: true,
         },
       ],
     };
-    await firstValueFrom(
-      this.http.post(`${this.keycloakUrl}/admin/realms/${realm}/users`, user),
-    );
+    const baseUrl = `${this.keycloakUrl}/admin/realms/${user.realm}/users`;
+    await firstValueFrom(this.http.post(`${baseUrl}/users`, newUser));
+
+    // send account verification email
     const keycloakUser = await firstValueFrom(
       this.http
-        .get(
-          `${this.keycloakUrl}/admin/realms/${realm}/users?username=${username}`,
-        )
-        .pipe(map((res) => res.data)),
+        .get(`${baseUrl}/users?username=${username}`)
+        .pipe(map((res) => res.data[0])),
     );
     await firstValueFrom(
       this.http.put(
-        `${this.keycloakUrl}/admin/realms/${realm}/users/${keycloakUser[0].id}/execute-actions-email?client_id=${client}&redirect_uri=`,
+        `${baseUrl}/users/${keycloakUser.id}/execute-actions-email?client_id=${user.client}&redirect_uri=`,
         ['VERIFY_EMAIL'],
       ),
     );
+
+    // assign default `user_app` role
     const role = await firstValueFrom(
-      this.http
-        .get(`${this.keycloakUrl}/admin/realms/${realm}/roles/user_app`)
-        .pipe(map((res) => res.data)),
+      this.http.get(`${baseUrl}/roles/user_app`).pipe(map((res) => res.data)),
     );
     await firstValueFrom(
       this.http.post(
-        `${this.keycloakUrl}/admin/realms/${realm}/users/${keycloakUser[0].id}/role-mappings/realm`,
+        `${baseUrl}/users/${keycloakUser.id}/role-mappings/realm`,
         [role],
       ),
     );
@@ -75,8 +90,8 @@ export class AccountController {
 
   @ApiOperation({
     summary: 'Set email of a user',
-    description: `Set or update the email of a registered user.
-      The email is updated for the user associated with the Bearer token.
+    description: `Set or update the email of a registered user. 
+      The email is updated for the user associated with the Bearer token. 
       This sends a verification email.
     `,
   })
