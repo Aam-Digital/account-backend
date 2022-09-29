10394a2b3f17e4ca14c4e9fa73490bc0b0d9779e
Simon <simon@aam-digital.com>
2022-09-29 14:02:19 +0200

    extracted keycloak http calls to service

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -15,18 +15,15 @@ import {
   catchError,
   concatMap,
   firstValueFrom,
-  forkJoin,
   map,
   tap,
   throwError,
 } from 'rxjs';
-import { HttpService } from '@nestjs/axios';
 import { ForgotEmailReq } from './forgot-email-req.dto';
 import { SetEmailReq } from './set-email-req.dto';
 import { User } from '../auth/user.dto';
-import { ConfigService } from '@nestjs/config';
 import { NewAccount } from './new-account.dto';
-import { KeycloakUser } from './keycloak-user';
+import { AccountService } from './account.service';
 
 /**
  * Endpoints to perform user account related tasks.
@@ -41,10 +38,7 @@ export class AccountController {
    */
   static readonly ACCOUNT_MANAGEMENT_ROLE = 'account_manager';
 
-  private readonly keycloakUrl: string;
-  constructor(private http: HttpService, configService: ConfigService) {
-    this.keycloakUrl = configService.get('KEYCLOAK_URL');
-  }
+  constructor(private accounts: AccountService) {}
 
   @ApiOperation({
     summary: 'create a new account',
@@ -58,6 +52,7 @@ export class AccountController {
   @Put()
   createAccount(@Req() req, @Body() { username, email, roles }: NewAccount) {
     const user = req.user as User;
+    const { realm, client } = user;
     if (
       !user['_couchdb.roles'].includes(
         AccountController.ACCOUNT_MANAGEMENT_ROLE,
@@ -65,33 +60,16 @@ export class AccountController {
     ) {
       return throwError(() => new UnauthorizedException('missing permissions'));
     }
-    // create user
-    const newUser = new KeycloakUser(username, email);
-    const baseUrl = `${this.keycloakUrl}/admin/realms/${user.realm}`;
     let userId: string;
     // create user
-    return this.http.post(`${baseUrl}/users`, newUser).pipe(
-      // send verification email
-      concatMap(() => this.http.get(`${baseUrl}/users?username=${username}`)),
-      tap((res) => (userId = res.data[0].id)),
+    return this.accounts.createUser(realm, username, email).pipe(
+      concatMap(() => this.accounts.findUserBy(realm, { username })),
+      tap((res) => (userId = res[0].id)),
       concatMap(() =>
-        this.http.put(
-          `${baseUrl}/users/${userId}/execute-actions-email?client_id=${user.client}&redirect_uri=`,
-          ['VERIFY_EMAIL'],
-        ),
+        this.accounts.sendEmail(realm, client, userId, 'VERIFY_EMAIL'),
       ),
-      // assign roles
-      concatMap(() =>
-        forkJoin(
-          roles.map((role) => this.http.get(`${baseUrl}/roles/${role}`)),
-        ),
-      ),
-      concatMap((res) =>
-        this.http.post(
-          `${baseUrl}/users/${userId}/role-mappings/realm`,
-          res.map((r) => r.data),
-        ),
-      ),
+      concatMap(() => this.accounts.getRoles(user.realm, roles)),
+      concatMap((res) => this.accounts.assignRoles(user.realm, userId, res)),
       catchError((err) => {
         throw new HttpException(
           err.response.data.errorMessage,
@@ -114,14 +92,18 @@ export class AccountController {
   @Put('set-email')
   async setEmail(@Req() req, @Body() { email }: SetEmailReq) {
     const user = req.user as User;
-    const url = `${this.keycloakUrl}/admin/realms/${user.realm}/users/${user.sub}`;
     await firstValueFrom(
-      this.http.put(url, { email: email, requiredActions: ['VERIFY_EMAIL'] }),
+      this.accounts.updateUser(user.realm, user.sub, {
+        email: email,
+        requiredActions: ['VERIFY_EMAIL'],
+      }),
     );
     await firstValueFrom(
-      this.http.put(
-        `${url}/execute-actions-email?client_id=${user.client}&redirect_uri=`,
-        ['VERIFY_EMAIL'],
+      this.accounts.sendEmail(
+        user.realm,
+        user.client,
+        user.sub,
+        'VERIFY_EMAIL',
       ),
     );
     return { ok: true };
@@ -134,18 +116,17 @@ export class AccountController {
   })
   @Post('forgot-password')
   async forgotPassword(@Body() { email, realm, client }: ForgotEmailReq) {
-    const usersUrl = `${this.keycloakUrl}/admin/realms/${realm}/users`;
-    const getUserUrl = `${usersUrl}?q=email:${email}`;
     const usersWithEmail = await firstValueFrom(
-      this.http.get<any[]>(getUserUrl).pipe(map((res) => res.data)),
+      this.accounts.findUserBy(realm, { email }),
     );
     // TODO only verified/valid accounts should allow a password reset?
     if (usersWithEmail.length !== 1 || usersWithEmail[0].email !== email) {
       throw new BadRequestException(`Could not find user with email: ${email}`);
     }
     const user = usersWithEmail[0];
-    const resetPasswordUrl = `${usersUrl}/${user.id}/execute-actions-email?client_id=${client}&redirect_uri=`;
-    await firstValueFrom(this.http.put(resetPasswordUrl, ['UPDATE_PASSWORD']));
+    await firstValueFrom(
+      this.accounts.sendEmail(realm, client, user.id, 'UPDATE_PASSWORD'),
+    );
     return { ok: true };
   }
 }
--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,4 +1,71 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { KeycloakUser } from './keycloak-user';
+import { forkJoin, map, Observable } from 'rxjs';
 
 @Injectable()
-export class AccountService {}
+export class AccountService {
+  private readonly keycloakUrl: string;
+  constructor(private http: HttpService, configService: ConfigService) {
+    this.keycloakUrl = configService.get('KEYCLOAK_URL');
+  }
+
+  createUser(realm: string, username: string, email: string) {
+    return this.keycloakRequest(
+      this.http.post,
+      `/${realm}/users`,
+      new KeycloakUser(username, email),
+    );
+  }
+
+  updateUser(realm: string, userId: string, user: Partial<KeycloakUser>) {
+    return this.keycloakRequest(
+      this.http.put,
+      `${realm}/users/${userId}`,
+      user,
+    );
+  }
+
+  findUserBy(realm: string, params: { [key in string]: string }) {
+    return this.keycloakRequest<{ id: string; email: string }[]>(
+      this.http.get,
+      `/${realm}/users`,
+      { params },
+    );
+  }
+
+  sendEmail(realm: string, client: string, userId: string, action: string) {
+    return this.keycloakRequest(
+      this.http.put,
+      `${realm}/users/${userId}/execute-actions-email?client_id=${client}&redirect_uri=`,
+      [action],
+    );
+  }
+
+  getRoles(realm: string, roles: string[]) {
+    return forkJoin(
+      roles.map((role) =>
+        this.keycloakRequest(this.http.get, `${realm}/roles/${role}`),
+      ),
+    );
+  }
+
+  assignRoles(realm: string, userId: string, roles: any[]) {
+    return this.keycloakRequest(
+      this.http.post,
+      `${realm}/users/${userId}/role-mappings/realm`,
+      roles,
+    );
+  }
+
+  private keycloakRequest<
+    R,
+    A extends (...args) => Observable<{ data: R }> = (
+      ...args
+    ) => Observable<{ data: R }>,
+  >(func: A, ...args: Parameters<typeof func>): Observable<R> {
+    const url = `${this.keycloakUrl}/admin/realms/${args[0]}`;
+    return func(url, ...args.slice(1)).pipe(map((res) => res.data));
+  }
+}
