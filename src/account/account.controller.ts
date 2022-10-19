afaa9735797d5a568ae0d5171816955d897b40c4
Simon <simon@aam-digital.com>
2022-10-19 15:32:58 +0200

    fix: activate account email is sent to correct address

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -10,7 +10,7 @@ import {
 } from '@nestjs/common';
 import { BearerGuard } from '../auth/bearer/bearer.guard';
 import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
-import { concatMap, firstValueFrom, forkJoin, Observable, tap } from 'rxjs';
+import { concatMap, concatWith, firstValueFrom, Observable, tap } from 'rxjs';
 import { ForgotEmailReq } from './forgot-email-req.dto';
 import { SetEmailReq } from './set-email-req.dto';
 import { User } from '../auth/user.dto';
@@ -179,9 +179,8 @@ export class AccountController {
       );
     }
     // first update the user object, then run other observables
-    return forkJoin([
-      this.keycloak.updateUser(realm, userId, updatedUser),
-      ...observables,
-    ]).pipe(prepareResult());
+    return this.keycloak
+      .updateUser(realm, userId, updatedUser)
+      .pipe(concatWith(...observables), prepareResult());
   }
 }
